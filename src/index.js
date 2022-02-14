f3c744074eaf51e5fbed5388e06941c756054c46
hjkimGithub <rlaguswo3408@gmail.com>
2022-02-14 22:33:56 +0900

    react-redux todolist

--- src/index.js
+++ src/index.js
@@ -1,73 +1,12 @@
-import {createStore} from "redux";
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
+import {Provider} from "react-redux";
+import store from "./store";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const ADD_TODO="ADD_TODO";
-const DELETE_TODO="DELETE_TODO";
-
-const addToDo = (text) => {
-  return {
-    type: ADD_TODO,
-    text 
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE_TODO,
-    id
-  };
-};
-
-const reducer = (state = [], action) => {
-  switch(action.type) {
-    case ADD_TODO:
-      // return [...state, {text:action.text, id:Date.now()}];
-      return [{text:action.text, id:Date.now()}, ...state];
-    case DELETE_TODO:
-      return state.filter(toDo => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
-
-store.subscribe(() => console.log(store.getState()));
-
-const dispatchAddToDo = (text) => {
-  store.dispatch(addToDo(text));
-}
-
-const dispatchDeleteToDo = (e) => {
-  const id = parseInt(e.target.parentNode.id);
-  store.dispatch(deleteToDo(id));
-}
-
-const paintToDos = () => {
-  const toDos =store.getState();
-  ul.innerHTML = "";
-  toDos.forEach(toDo => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "DEL";
-    btn.addEventListener("click", dispatchDeleteToDo);
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-    ul.appendChild(li);
-    li.appendChild(btn);
-  })
-}
-
-store.subscribe(paintToDos)
-
-const onSubmit = (e) => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  dispatchAddToDo(toDo);
-}
-
-form.addEventListener("submit", onSubmit);
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
