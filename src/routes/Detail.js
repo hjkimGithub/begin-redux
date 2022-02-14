a556e06ecbd484571f527402c7595549dcd2ef24
hjkimGithub <rlaguswo3408@gmail.com>
2022-02-14 22:47:20 +0900

    detail(useParam, mapstatetoprops practice

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,20 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+
+function Detail({ toDos }) {
+    const myId = useParams().id;
+    const toDo = toDos.find((toDo) => toDo.id === parseInt(myId));
+    return (
+        <>
+        {toDo?.text}
+        Created at: {toDo?.id}
+        </>
+    );
+}
+
+function mapStateToProps(state) {
+    return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Detail);
